66afaab354afe8781bd862c1f0efa64c4979fbf8
Alunos <samuelpregnolatto@gmail.com>
2025-10-01 20:26:02 -0300

    fix(import): correct App import path and update dependencies to compatible versions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 import { registerRootComponent } from 'expo';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import App from './App.native';
+import App from './App';
 
 function RootApp() {
   return (
